7cfe917f353cd4eaa7943bd61cea327da652f4a5
Vladvs21 <vlad_v_s@ukr.net>
2023-09-24 19:31:58 +0300

    build main

--- main/src/test/java/com/example/financial_manager/FinancialManagerApplicationTests.java
+++ main/src/test/java/com/example/financial_manager/FinancialManagerApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.financial_manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FinancialManagerApplicationTests {
+// @SpringBootTest
+// class FinancialManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
