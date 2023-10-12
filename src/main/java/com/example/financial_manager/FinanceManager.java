0aa78913c6cbe1aec1f90d15034c86649766e202
wwowaa <vovanova2209@gmail.com>
2023-10-12 23:11:52 +0300

    Added MDC and Marker

--- src/main/java/com/example/financial_manager/FinanceManager.java
+++ src/main/java/com/example/financial_manager/FinanceManager.java
@@ -4,6 +4,9 @@ import com.example.financial_manager.managers.ExpenseManager;
 import com.example.financial_manager.managers.IncomeManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,6 +16,7 @@ public class FinanceManager {
     private final ExpenseManager expenseManager;
     private final IncomeManager incomeManager;
     private static final Logger logger = LoggerFactory.getLogger(FinanceManager.class);
+    private static final Marker SECURITY_MARKER = MarkerFactory.getMarker("SECURITY");
 
     @Value("${currency.default}")
     private String currency;
@@ -24,9 +28,12 @@ public class FinanceManager {
     }
 
     public double calculateBudget() {
+        MDC.put("currency", currency);
         double totalExpenses = expenseManager.calculateTotalExpenses();
         double totalIncome = incomeManager.calculateTotalIncome();
+        logger.info("Calculating budget for currency: {}", currency);
         logger.info(currency);
+        MDC.remove("currency");
         //System.out.println("FinanceManager currency: " + currency);
         return totalIncome - totalExpenses;
     }
--- src/main/java/com/example/financial_manager/components/ExpenseManagerImpl.java
+++ src/main/java/com/example/financial_manager/components/ExpenseManagerImpl.java
@@ -6,6 +6,8 @@ import com.example.financial_manager.entities.Expense;
 import com.example.financial_manager.repositories.ExpenseRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +21,7 @@ public class ExpenseManagerImpl implements ExpenseManager {
     private ExpenseRepository expenseRepository;
 
     private static final Logger logger = LoggerFactory.getLogger(FinanceManager.class);
+    private static final Marker SECURITY_MARKER = MarkerFactory.getMarker("SECURITY");
     @Override
     public void addExpense(double amount, String purpose) {
         expenseRepository.save(new Expense(null,amount,purpose));
@@ -37,6 +40,7 @@ public class ExpenseManagerImpl implements ExpenseManager {
                 Expense updatedExpense = expense.get();
                 updatedExpense.setExpanseAmount(amount);
                 expenseRepository.save(updatedExpense);
+                logger.info(SECURITY_MARKER, "Security check passed");
                 logger.info("Expanse amount: Successfully updated");
             }else{
                 logger.warn("Expanse amount: Not updated. Wrong id");
