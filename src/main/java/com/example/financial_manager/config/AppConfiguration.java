fd42189aa635c7571e3cd5f6fc70c3e2853f465d
Viktor <viktoravavd@gmail.com>
2023-10-26 16:12:47 +0300

    OpenApi spec, Expanse CRUD.postman_collection.json

--- src/main/java/com/example/financial_manager/config/AppConfiguration.java
+++ src/main/java/com/example/financial_manager/config/AppConfiguration.java
@@ -1,23 +1,15 @@
-package com.example.financial_manager;
+package com.example.financial_manager.config;
 
 import com.example.financial_manager.components.IncomeManagerImpl;
 import com.example.financial_manager.managers.IncomeManager;
-import com.example.financial_manager.mappers.ExpenseMapper;
 import com.example.financial_manager.mappers.IncomeMapper;
 import com.example.financial_manager.repositories.IncomeRepository;
-import com.example.financial_manager_starter.services.UserInfoService;
-import com.example.financial_manager_starter.services.UserInfoServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.mapstruct.factory.Mappers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @PropertySource("classpath:application.properties")
