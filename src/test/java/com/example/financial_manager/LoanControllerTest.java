c962e34d47e005e4c9fb61842637f5473e0cf478
Володимир <vovanovak2209@gmail.com>
2023-12-14 17:52:40 +0200

    Created integrated tests for existing controllers

---
+++ src/test/java/com/example/financial_manager/LoanControllerTest.java
@@ -0,0 +1,116 @@
+package com.example.financial_manager;
+
+import com.example.financial_manager.components.LoanManagerImpl;
+import com.example.financial_manager.components.LoanManagerImpl;
+import com.example.financial_manager.controllers.LoanController;
+import com.example.financial_manager.controllers.LoanController;
+import com.example.financial_manager.dto.LoanDto;
+import com.example.financial_manager.securityConfig.SecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(LoanController.class)
+@Import(SecurityConfig.class)
+public class LoanControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @MockBean
+    private LoanManagerImpl loanManager;
+
+    @Autowired
+    private WebApplicationContext context;
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "password", roles = "USER")
+    public void getAllLoans() throws Exception {
+        mockMvc.perform(get("/loans")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "password", roles = "USER")
+    public void getLoanById() throws Exception {
+        mockMvc.perform(get("/loans/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", password = "1111", roles = "ADMIN")
+    public void createLoan() throws Exception {
+        LoanDto loanDto = new LoanDto(null, "Derek", 100);
+        LoanDto createdLoan = new LoanDto(3L, "Part-Time Job", 100);
+
+        when(loanManager.addLoan(any())).thenReturn(createdLoan);
+
+        mockMvc.perform(post("/loans/createLoan")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(objectMapper.writeValueAsString(loanDto)))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(objectMapper.writeValueAsString(createdLoan)));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", password = "1111", roles = "ADMIN")
+    public void updateLoan() throws Exception {
+        LoanDto updatedLoan = new LoanDto(1L, "Monobank", 100);
+
+        when(loanManager.updateLoan(eq(1L), any())).thenReturn(updatedLoan);
+
+        mockMvc.perform(put("/loans/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(objectMapper.writeValueAsString(updatedLoan)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(updatedLoan)));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", password = "1111", roles = "ADMIN")
+    public void deleteLoan() throws Exception {
+        mockMvc.perform(delete("/loans/1")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isNoContent());
+    }
+}
